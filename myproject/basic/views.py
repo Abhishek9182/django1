2a5477159c5d2cee82570c1284aae32916df3a80
Abhishek9182 <reddyabhishek9182@gmail.com>
2025-11-23 00:16:12 +0530

    intialstep

---
+++ myproject/basic/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+
+
+# Create your views here.
+def sample(request):
+    return HttpResponse("welcome to django")
+
+def basic(request):
+    return HttpResponse("hey dude! Whats-app")
+
+def calc(request):
+    a=int(request.GET.get('a'))
+    b=int(request.GET.get('b'))
+    op=request.GET.get('op')
+    if(op=='+'):
+        return HttpResponse(a+b)
+    elif(op=='-'):
+        return HttpResponse(a-b)
+    elif(op=='*'):
+        return HttpResponse(a*b)
+    elif(op=='/'):
+        return HttpResponse(a/b)
+    elif(op=='%'):
+        return HttpResponse(a%b)
+    else:
+        return HttpResponse('invalid operator')
